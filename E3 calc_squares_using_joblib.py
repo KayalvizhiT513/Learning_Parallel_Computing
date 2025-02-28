41ad42bc85aadcabf321d5cd9788574d7e81e0d9
Kayalvizhi T <95753452+KayalvizhiT513@users.noreply.github.com>
2025-02-28 16:41:30 +0530

    Update and rename calc_squares_using_joblib.py to E3 calc_squares_using_joblib.py

--- E3 calc_squares_using_joblib.py
+++ E3 calc_squares_using_joblib.py
@@ -5,7 +5,7 @@ def random_square(seed):
     np.random.seed(seed)
     random_num = np.random.randint(0, 10)
     return random_num**2
-'''
+
 print("With 8 jobs")
 results = Parallel(n_jobs=8, verbose=1)\
           (delayed(random_square)(i) for i in range(1000000))
@@ -17,7 +17,7 @@ results = Parallel(n_jobs=-1, verbose=1)\
 print("Using all CPUs but one")
 results = Parallel(n_jobs=-2, verbose=1)\
           (delayed(random_square)(i) for i in range(1000000))
-'''
+
 print("Backend set to multiprocessing")
 results = Parallel(n_jobs=-1, backend="multiprocessing", verbose=1)\
           (delayed(random_square)(i) for i in range(1000000))
